9e9d2b2a3d242d72bb0ae0d38e43dd9ccbdf7c4d
laslo76 <bogushev.v@yandex.com>
2026-01-05 09:24:31 +0300

    Fixed the description of the types in the 4th line main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val likes: Int = 1111
+    val likes: Int = 1121
 
-    val postfix:String = if (likes % 10 == 1 && likes % 100 != 11) "человеку"  else "людям"
+    val postfix = if (likes % 10 == 1 && likes % 100 != 11) "человеку"  else "людям"
     println("Понравилось $likes $postfix")
 }
