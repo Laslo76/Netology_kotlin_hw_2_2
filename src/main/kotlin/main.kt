73f748955316c78d0f3e133969a8e6d2fea8f0d4
laslo76 <bogushev.v@yandex.com>
2026-01-08 10:54:33 +0300

    The file was tested on: 1, 11, 41,1121 ..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val likes: Int = 1121
+    val likes = 1121
 
     val postfix = if (likes % 10 == 1 && likes % 100 != 11) "человеку"  else "людям"
     println("Понравилось $likes $postfix")
